cf3fb4b89192ceedd3bad1e343bba94b7d04d470
Christhonie Geldenhuys <development@christhonie.co.za>
2021-07-02 15:49:16 +0200

    Add a data source

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { products } from '../products';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products;
+
+  constructor() {
+    this.products = products;
+  }
 
   ngOnInit(): void {
   }
 
-}
+  }
